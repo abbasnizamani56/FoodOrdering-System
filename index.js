6591fd950ed8bc31ee9f69eb37b433a0bec546ed
Muhammad Abbas <127150090+abbasnizamani56@users.noreply.github.com>
2025-04-23 23:50:27 +0500

    Add files via upload
    These are all files containing the details for food ordering website in which admin.js is the main file andthe model files which contain different models. Also there are routes files which make frontend pages changed from one page to another page.

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const router = express.Router()
+const controller = require("../controller/index")
+const  auth = require("../middleware/auth-middleware")
+    
+router.use(
+    "/admin",
+    require("./admin-route")
+)
+
+router.use(
+    "/food",
+    require("./food-route")
+)
+
+router.use(
+    "/user/cart",
+    require("./cart-route")
+)
+
+router.use(
+    "/user/ordering",
+    require("./ordering-route")
+)
+
+router.get(
+    "/",
+    controller.allFoods
+)
+
+router.post(
+    "/signup",
+    controller.newUser
+)
+    
+router.post(
+    "/login",
+    controller.login
+)
+    
+router.post(
+    "/edit",
+    auth.decodeToken,
+    controller.userUpdate
+)
+    
+router.post(
+    "/updatepassword",
+    auth.decodeToken,
+    controller.updatePassword
+)
+    
+router.post(
+    "/requestpasswordreset",
+    auth.decodeToken,
+    controller.requestPasswordReset
+)
+    
+router.post(
+    "/resetpassword",
+    auth.decodeToken,
+    controller.resetPassword
+)
+router.get(
+    "/:id",
+    auth.decodeToken,
+    controller.userProfile
+)
+
+router.delete(
+    "/:id",
+    auth.decodeToken,
+    controller.delUser
+)
+module.exports = router
